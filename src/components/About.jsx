b723d43a74b8380e41958d0841afbec5af83e659
Hasan Mahi <uddinhasan930@gmail.com>
2025-06-04 08:03:34 +0400

    fixed mejor

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
       <div className="container">
         <h2 className="text-center mb-4">About Us</h2>
         <p className="lead text-center mx-auto" style={{ maxWidth: '700px' }}>
-          Nidham Alauddin Workshop has been a trusted name in auto repair for over 45 years. Our mechanics are dedicated to providing honest, efficient, and affordable services—from diagnostics to full repairs. Customer satisfaction and vehicle safety are our top priorities.
+          Nidham Alauddin Workshop has been a trusted name in auto repair for over 40 years. Our mechanics are dedicated to providing honest, efficient, and affordable services—from diagnostics to full repairs. Customer satisfaction and vehicle safety are our top priorities.
         </p>
       </div>
     </section>
